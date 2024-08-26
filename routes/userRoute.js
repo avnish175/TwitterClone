50b30e3ac220debc4d79aa4d4df1692c2d4ecb17
avnish175 <125601430+avnish175@users.noreply.github.com>
2024-08-26 21:25:35 +0530

    final

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,14 @@
 import express from "express";
-import { Login, Register, bookmark, follow, getMyProfile, getOtherUsers, logout, unfollow } from "../controllers/userController.js";
+import {
+  Login,
+  Register,
+  bookmark,
+  follow,
+  getMyProfile,
+  getOtherUsers,
+  logout,
+  unfollow,
+} from "../controllers/userController.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
@@ -7,7 +16,7 @@ const router = express.Router();
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/logout").get(logout);
-router.route("/bookmark/:id").put(isAuthenticated, bookmark)
+router.route("/bookmark/:id").put(isAuthenticated, bookmark);
 router.route("/profile/:id").get(isAuthenticated, getMyProfile);
 router.route("/otheruser/:id").get(isAuthenticated, getOtherUsers);
 router.route("/follow/:id").post(isAuthenticated, follow);
